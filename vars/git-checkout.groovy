665c9b093462aa42c103cfca956814a4651dd129
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-10 14:37:53 +0530

    Update git-checkout.groovy

--- vars/git-checkout.groovy
+++ vars/git-checkout.groovy
@@ -1,4 +1,16 @@
 def call(def giturl, def branch, def credentialId) {
 //	git branch: '${branch}', credentialsId: '${credentialId}', url: '${giturl}'
-	git([url: ${giturl}, branch: ${branch}, credentialsId: ${credentialId}])
+//	git([url: ${giturl}, branch: ${branch}, credentialsId: ${credentialId}])
+
+checkout([$class: 'GitSCM', 
+    branches: [[name: '*/master']], 
+    doGenerateSubmoduleConfigurations: false, 
+    extensions: [[$class: 'CleanCheckout']], 
+    submoduleCfg: [], 
+    userRemoteConfigs: [[credentialsId: '5bd19c27-30a1-42d9-9f82-2d62eb81e2ac', url: 'https://github.com/vernekarakshata/Inventory-Jenkins.git']]
+])
+
+
+
+
 }
