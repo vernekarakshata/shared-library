d081c3467e1f1f37845199407ee6f84e311918d7
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-10 14:24:48 +0530

    Update git-checkout.groovy

--- vars/git-checkout.groovy
+++ vars/git-checkout.groovy
@@ -1,3 +1,4 @@
 def call(def giturl, def branch, def credentialId) {
-	git branch: ${branch}, credentialsId: ${credentialId}, url: ${giturl}
+	git branch: '${branch}', 
+	    credentialsId: '${credentialId}', url: '${giturl}'
 }
