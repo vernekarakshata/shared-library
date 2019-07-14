84032dc4c0154d9f16870025b426c2f74660443e
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-14 13:26:57 +0530

    Update sendMail.groovy

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -1,1 +1,32 @@
-
+def call(def emailTemplate, def subject, def to, def from, def cc, def bcc, def replyTo) {
+	echo "Hello ${name}"
+  
+  mail bcc: '', 
+       body: "${emailTemplate}",
+       cc: "${cc}",
+       charset: 'UTF-8',
+       from: "${from}",
+       mimeType: 'text/html',
+       replyTo: "${replyTo}",
+       subject: "${subject}", 
+       to: "${to}";
+  
+  
+  
+  
+  /*
+  mail bcc: '', 
+            body: "<b>Project Build Status</b>" + 
+                "<br><br>Please find below the project details:" +
+                "<table>" + 
+                "<tr><td>Project</td><td>${env.JOB_NAME}</td></tr>" +
+                "<tr><td>Build Number</td><td>${env.BUILD_NUMBER}</td></tr>" +
+                "<tr><td>Project Deployment URL</td><td>${env.BUILD_URL}</td></tr></table>",
+            cc: '', 
+            charset: 'UTF-8', 
+            from: '', 
+            mimeType: 'text/html', 
+            replyTo: '', 
+            subject: "Project ${env.JOB_NAME} deployment '${currentBuild.result}'", 
+            to: "akshatav10@gmail.com";*/
+}
