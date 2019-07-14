b8795d03d2febf856b2b554e3e4549e202f6fa4b
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-14 14:50:14 +0530

    Update mavenBuildArtifactoryDeployment.groovy

--- vars/mavenBuildArtifactoryDeployment.groovy
+++ vars/mavenBuildArtifactoryDeployment.groovy
@@ -1,39 +1,14 @@
 def call(def maventool, def pom, def mavenGoals, def artifactoryServerId,  
           def resolverId, def resolverReleaseRepo, def resolversnapshotRepo,
           def deployerId, def deployerReleaseRepo, def deployersnapshotRepo) {
-          
-          
-  echo "maventool ${maventool}"
-  echo "pom ${pom}"
-  echo "mavenGoals ${mavenGoals}"
-  echo "artifactoryServerId ${artifactoryServerId}"
-  echo "resolverId ${resolverId}"
-  echo "resolverReleaseRepo ${resolverReleaseRepo}"
-  echo "resolversnapshotRepo ${resolversnapshotRepo}"
-  echo "deployerId ${deployerId}"
-  echo "deployerReleaseRepo ${deployerReleaseRepo}"
-  echo "deployersnapshotRepo ${deployersnapshotRepo}"
-  
-  /*
-  def server = Artifactory.server "${artifactoryServerId}"
-  def rtMaven = Artifactory.newMavenBuild()
-  def buildInfo
-  rtMaven.tool = "${maventool}" 
-  rtMaven.deployer releaseRepo: '"${deployerReleaseRepo}"', snapshotRepo: '"${deployersnapshotRepo}"', server: server
-  rtMaven.resolver releaseRepo: '"${resolverReleaseRepo}"', snapshotRepo: '"${resolversnapshotRepo}"', server: server
-  buildInfo = Artifactory.newBuildInfo()
-  rtMaven.run pom: "${pom}", goals: '"${mavenGoals}"', buildInfo: buildInfo
-  server.publishBuildInfo buildInfo
-  */
-
-
+   /*       
   rtMavenResolver (
     id: "${resolverId}",
     serverId: "${artifactoryServerId}",
     releaseRepo: "${resolverReleaseRepo}",
     snapshotRepo: "${resolversnapshotRepo}"
   )  
-  
+  */
 
  
   rtMavenDeployer (
@@ -49,7 +24,7 @@ def call(def maventool, def pom, def mavenGoals, def artifactoryServerId,
     goals: "${mavenGoals}",
     // Maven options.
     opts: '-Xms1024m -Xmx4096m',
-    resolverId: "${resolverId}",
+//    resolverId: "${resolverId}",
     deployerId: "${deployerId}",
   )
   
