efc79ba1cba177afcd39e81023a45eebfccc9c56
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-14 16:04:47 +0530

    Update deployToServer.groovy

--- vars/deployToServer.groovy
+++ vars/deployToServer.groovy
@@ -1,3 +1,4 @@
 def call(def source, def user, def hostname, def destination) {
+    echo "scp ${source} ${user}@${hostname}:${destination}"
     sh "scp ${source} ${user}@${hostname}:${destination}"
 }
