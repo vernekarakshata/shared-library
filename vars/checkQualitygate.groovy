1e1f8eff07f8039f103afce2fb6c7941f096ccc0
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-14 12:40:56 +0530

    added defination for quality gate

--- vars/checkQualitygate.groovy
+++ vars/checkQualitygate.groovy
@@ -1,1 +1,9 @@
-
+def call() {
+  timeout(time: 1, unit: 'HOURS') {
+    def qg = waitForQualityGate()
+    
+    if (qg.status != 'OK') {
+      error "Quality Gate failure: ${qg.status}"
+    }
+  }
+}
