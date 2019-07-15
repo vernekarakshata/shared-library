d77ce23eaa7d3647e49771e7ce328c5ee8fba860
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-15 11:32:15 +0530

    Update uploadFromArtifactory.groovy

--- vars/uploadFromArtifactory.groovy
+++ vars/uploadFromArtifactory.groovy
@@ -1,1 +1,13 @@
-
+def call(def pattern, def downloadDir, def serverID) {
+  def server = "${serverID}"
+  def downloadSpec = """{
+       "files": [
+          {
+              "pattern": "${pattern}",
+              "target": "${downloadDir}",
+              "flat":true
+          }
+      ]
+  }"""
+  server.download(downloadSpec)
+}
