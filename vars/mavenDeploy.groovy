ef3bd92153c3beb600c1fdf364ddf0a885d10878
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-15 12:59:34 +0530

    Update mavenDeploy.groovy

--- vars/mavenDeploy.groovy
+++ vars/mavenDeploy.groovy
@@ -1,1 +1,24 @@
-
+def call(def maventool, def pom, def mavenGoals, def artifactoryServerId,  
+          def deployerId, def deployerReleaseRepo, def deployersnapshotRepo) {
+         
+  rtMavenDeployer (
+      id: "${deployerId}",
+      serverId: "${artifactoryServerId}",
+      releaseRepo: "${deployerReleaseRepo}",
+      snapshotRepo: "${deployersnapshotRepo}"
+  )
+  
+  rtMavenRun (
+    tool: "${maventool}",
+    pom: "${pom}",
+    goals: "${mavenGoals}",
+    opts: '-Xms1024m -Xmx4096m',
+    deployerId: "${deployerId}",
+  )
+  
+  rtPublishBuildInfo (
+    serverId: "${artifactoryServerId}"
+)
+  
+ 
+}
