d08ac219b2d3529566e19a4eb943711c77c4f64c
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-15 12:18:11 +0530

    Update mavenBuildArtifactoryDeployment.groovy

--- vars/mavenBuildArtifactoryDeployment.groovy
+++ vars/mavenBuildArtifactoryDeployment.groovy
@@ -1,16 +1,6 @@
 def call(def maventool, def pom, def mavenGoals, def artifactoryServerId,  
-          def resolverId, def resolverReleaseRepo, def resolversnapshotRepo,
           def deployerId, def deployerReleaseRepo, def deployersnapshotRepo) {
-        
-  rtMavenResolver (
-    id: "${resolverId}",
-    serverId: "${artifactoryServerId}",
-    releaseRepo: "${resolverReleaseRepo}",
-    snapshotRepo: "${resolversnapshotRepo}"
-  )  
-  
-
- 
+         
   rtMavenDeployer (
       id: "${deployerId}",
       serverId: "${artifactoryServerId}",
