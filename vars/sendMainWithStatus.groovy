9035f9a11712691fd1301f31e47f2f6897e3228a
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-16 17:34:52 +0530

    Create sendMainWithStatus.groovy

---
+++ vars/sendMainWithStatus.groovy
@@ -0,0 +1,20 @@
+def call(def to, def from, def cc, def bcc, def replyTo, def result) {
+//	echo "Hello ${emailTemplate}"
+  
+  mail bcc: '', 
+       body: "<b>Project Build Status</b>" + 
+                "<br><br>Please find below the project details:" +
+                "<table>" + 
+                "<tr><td>Project</td><td>${env.JOB_NAME}</td></tr>" +
+                "<tr><td>Build Number</td><td>${env.BUILD_NUMBER}</td></tr>" +
+                "<tr><td>Project Deployment URL</td><td>${env.BUILD_URL}</td></tr></table>",
+       cc: "${cc}",
+       charset: 'UTF-8',
+       from: "${from}",
+       mimeType: 'text/html',
+       replyTo: "${replyTo}",
+       subject: "Project ${env.JOB_NAME} deployment ${result}", 
+       to: "${to}";
+  
+  
+}
