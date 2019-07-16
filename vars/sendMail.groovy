50541ed6d3be321fe9b04be224aaeb5524d6c717
vernekarakshata <48825589+vernekarakshata@users.noreply.github.com>
2019-07-16 17:22:44 +0530

    Update sendMail.groovy

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -13,7 +13,7 @@ def call(def to, def from, def cc, def bcc, def replyTo) {
        from: "${from}",
        mimeType: 'text/html',
        replyTo: "${replyTo}",
-       subject: "Project ${env.JOB_NAME} deployment '${currentBuild.currentResult}'", 
+       subject: "Project ${env.JOB_NAME} deployment ${currentBuild.result}", 
        to: "${to}";
   
   
